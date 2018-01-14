cb50a36e16b883d30accca747ef96648f4d5e16f
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-14 14:10:44 -0800

    Use componentDidMount lifecycle method to call fetchPosts get request

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,15 +1,23 @@
 // import React and Component from react
+// import connect from react-redux
+// import fetchPosts action creator from /actions
 
 // class PostsIndex extends Component
+	// use componentDidMount lifecycle method as will be called as soon as rendered to dom            
 	// render
 		// return
 			// div simple placeholder text
 
-// export default PostsIndex
+// export default connect null, {fetchPosts} PostsIndex instead of using mapDispatchToProps
 
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
+	componentDidMount(){
+		this.props.fetchPosts();
+	}
 	render(){
 		return(
 			<div>Posts Index</div>
@@ -17,4 +25,4 @@ class PostsIndex extends Component {
 	}
 }
 
-export default PostsIndex;
+export default connect(null, { fetchPosts })(PostsIndex);
