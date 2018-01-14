c50bda64b5effbba9a9ef5603a53f33316ba4d41
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-14 13:38:37 -0800

    Correct PostsIndex import typo

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import PostIndex from './components/posts_index';
+import PostsIndex from './components/posts_index';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
