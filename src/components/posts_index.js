8a7fda12d589d37900af52421988a63542e351f9
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-14 13:31:45 -0800

    Implement PostsIndex component rendering simple text div

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -6,3 +6,15 @@
 			// div simple placeholder text
 
 // export default PostsIndex
+
+import React, { Component } from 'react';
+
+class PostsIndex extends Component {
+	render(){
+		return(
+			<div>Posts Index</div>
+		);
+	}
+}
+
+export default PostsIndex;
