90dafbb160be9a847eb88e618fdbf339de365ab7
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-14 13:52:38 -0800

    Import axios, create root url and api key constants, use those to create a url for axios get request, pass that into action payload

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,27 @@
+// import axios from axios
+
 // export const FETCH_POSTS stores fetch_posts string
 
+// create const to hold root url
+// create const to hold api key
+
 // export first action creator fetchPosts function
+	// const request set to axios.get and use redux blog api info to fill template url
 	// return
 		// type FETCH_POSTS
-		// payload
+		// payload set to request
+
+import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
 
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+const API_KEY = '?key=seabassgonzalez02';
+
 export function fetchPosts(){
+	const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
 	return{
 		type: FETCH_POSTS
+		payload: request
 	};
 }
