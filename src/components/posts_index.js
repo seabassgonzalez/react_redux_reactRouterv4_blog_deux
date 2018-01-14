5cae4181232d051cea543dabbf56e9fcf21d6e63
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-14 14:33:35 -0800

    Implement helper function renderPosts which uses lodash .map method to map over posts and pass each to function that creates an li for each

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,9 +1,14 @@
 // import React and Component from react
 // import connect from react-redux
 // import fetchPosts action creator from /actions
+// import _ from lodash
 
 // class PostsIndex extends Component
 	// use componentDidMount lifecycle method as will be called as soon as rendered to dom            
+	// renderPosts
+		// returns map over object containing posts using lodash, function that takes each post and makes an li for each
+			// li className list-group-item key set to reference to post.id
+				// reference to post.title
 	// render
 		// return
 			// div 
@@ -19,10 +24,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
+import _ from 'lodash';
 
 class PostsIndex extends Component {
 	componentDidMount(){
 		this.props.fetchPosts();
+	}
+	renderPosts(){
+		return _.map(this.props.posts, post => {
+			return(
+				<li className="list-group-item" key={post.id}>
+					{post.title}
+				</li> // comment to clean up syntax highlighting
+			);
+		});
 	}
 	render(){
 		console.log('posts are: ', this.props.posts);
