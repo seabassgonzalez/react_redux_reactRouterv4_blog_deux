de72547017e9ee830b3742c05791ce3b8132d662
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-14 13:59:12 -0800

    Implement posts reducer

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -8,3 +8,12 @@
 			// return state object
 
 import { FETCH_POSTS } from '../actions';
+
+export default function(state = {}, action){
+	switch(action.type){
+		case FETCH_POSTS:
+			console.log(action.payload.data);
+		default:
+			return state;
+	}
+}
