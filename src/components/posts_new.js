93cc57c4c5a071a1ac4c5a134b49efd039181f65
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-14 19:12:14 -0800

    Import form helpers from redux-form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,4 +1,5 @@
 // import React and Component from react
+// import Field and reduxForm from redux-form
 
 // class PostsNew extends Component
 	// render
@@ -8,6 +9,7 @@
 // export default PostsNew
 
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component{
 	render(){
