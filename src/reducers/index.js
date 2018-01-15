adf6dca41ec5348f20c56ee51b14c60029d351a7
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-14 18:19:39 -0800

    Install redux-form, import reducer and set to form key of rootReducer combineReducers call

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,16 +1,20 @@
 // import combineReducers from redux
 // import PostsReducer from reducer_posts
+// import reducer from redux-form, import as formReducer
 
 // create const rootReducer set to call to combineReducers 
 	// assign posts piece of state to PostsReducer
+	// assign formReducer to key form
 
 // export default rootReducer
 
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import PostsReducer from './reducer_posts';
 
 const rootReducer = combineReducers({
-	posts: PostsReducer
+	posts: PostsReducer,
+	form: formReducer
 });
 
 export default rootReducer;
