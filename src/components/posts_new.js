94d4c5b5794ba2a908ce6a03ff28ab6bb0aea0fb
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-17 15:52:55 -0800

    Import create_post action creator, connect from react-redux, stack multiple connect-like helpers in export of reduxForm

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -50,6 +50,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions/index';
 
 class PostsNew extends Component{
 	renderField(field){
@@ -118,4 +120,6 @@ function validate(values){
 export default reduxForm({
 	validate,
 	form: 'PostsNewForm'
-})(PostsNew);
+})(
+	connect(null, { createPost })(PostsNew)
+	;
