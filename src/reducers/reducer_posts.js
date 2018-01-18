b48d02ebaac15ba68fcce65d67462d422bb5c775
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-18 12:20:11 -0800

    Import DELETE_POST action creator, create case for delete post, use underscore .omit method to delete action.payload -- in this case id of post to delete -- from state

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,7 +1,9 @@
-// import FETCH_POSTS and FETCH_POST from actions
+// import FETCH_POSTS FETCH_POST and DELETE_POST from actions
 
 // export default function taking state defaulting it to an object and an action
 	// switch statement for action types
+		// DELETE_POST
+			// return _.omit passing in state, and action.payload -- look at state object, if it has key of post id, just drop it omit it and return new object without that id
 		// FETCH_POST
 			// return state object ...state to save posts, use key interpolation with [] make action.payload.id into object with key to id and set value to action.payload.data
 		// FETCH_POSTS
@@ -10,10 +12,12 @@
 			// return state object
 
 import _ from 'lodash';
-import { FETCH_POSTS, FETCH_POST } from '../actions';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
 
 export default function(state = {}, action){
 	switch(action.type){
+		case DELETE_POST: // action payload contains id of post just deleted
+			return _.omit(state, action.payload);
 		case FETCH_POST:
 			return { ...state, [action.payload.data.id] : action.payload.data };
 		case FETCH_POSTS:
