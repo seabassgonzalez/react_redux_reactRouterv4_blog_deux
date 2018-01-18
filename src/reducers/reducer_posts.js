70524332a1e3d4bfdd24b5a841cccb25fa0d0dec
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-17 17:14:05 -0800

    Implement es6 key interpolation and syntactic sugar to condense, still fetching each additional post and add them to overall state object

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -3,10 +3,7 @@
 // export default function taking state defaulting it to an object and an action
 	// switch statement for action types
 		// FETCH_POST
-			// save post in const from action.payload.data
-			// store state object as newState
-			// set post to newState with new property [post.id]
-			// return newState
+			// return state object ...state to save posts, use key interpolation with [] make action.payload.id into object with key to id and set value to action.payload.data
 		// FETCH_POSTS
 			// return _.mapKeys on array action.payload.data, use id prop to create id
 		// default case
@@ -18,10 +15,7 @@ import { FETCH_POSTS, FETCH_POST } from '../actions';
 export default function(state = {}, action){
 	switch(action.type){
 		case FETCH_POST:
-			const post = action.payload.data;
-			const newState = { ...state };
-			newState[post.id] = post;
-			return newState;
+			return { ...state, [action.payload.data.id] : action.payload.data };
 		case FETCH_POSTS:
 			return _.mapKeys(action.payload.data, 'id');
 		default:
