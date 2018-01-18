470bd13645469f39d8c968101eb882a373340db9
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-17 16:35:17 -0800

    Implement Posts Show component

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -6,3 +6,15 @@
 			// div simply saying Post Show
 
 // export default PostsShow
+
+import React, { Component } from 'react';
+
+class PostsShow extends Component {
+	render(){
+		return(
+			<div>
+				Posts Show
+			</div>
+		);
+	}
+}
