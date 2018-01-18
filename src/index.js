d8814b3c103d00f0a4ebe41bdf7be68a59c840d3
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-17 16:37:37 -0800

    Import PostsShow component, implement new Route with variable id for a given post in path

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@
 // import reducers
 // import PostsIndex from './components/posts_index'
 // import PostsNew from components/posts_new
+// import PostsShow from components/posts_show
 
 // create const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore)
 
@@ -31,6 +32,7 @@ import promise from 'redux-promise';
 
 import PostsIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
+import PostsShow from './components/posts_show';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
@@ -41,7 +43,8 @@ ReactDOM.render(
   		<div>
   			<Switch>
 	  			<Route path="/posts/new" component={PostsNew} />
-	  			<Route path="/" component={PostsIndex} />
+	  			<Route path="/posts/:id" component={PostsShow} /> 
+	  			<Route path="/" component={PostsIndex} /> 																		
 	  		</Switch>
 	  	</div>
   	</BrowserRouter>
