468c479a93bcaacad40e3cd7f71952d2f4f9f437
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-17 17:10:06 -0800

    Make sure object includes id as key for post on state

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -3,7 +3,10 @@
 // export default function taking state defaulting it to an object and an action
 	// switch statement for action types
 		// FETCH_POST
-			// return
+			// save post in const from action.payload.data
+			// store state object as newState
+			// set post to newState with new property [post.id]
+			// return newState
 		// FETCH_POSTS
 			// return _.mapKeys on array action.payload.data, use id prop to create id
 		// default case
@@ -15,7 +18,10 @@ import { FETCH_POSTS, FETCH_POST } from '../actions';
 export default function(state = {}, action){
 	switch(action.type){
 		case FETCH_POST:
-			return { ...state };
+			const post = action.payload.data;
+			const newState = { ...state };
+			newState[post.id] = post;
+			return newState;
 		case FETCH_POSTS:
 			return _.mapKeys(action.payload.data, 'id');
 		default:
