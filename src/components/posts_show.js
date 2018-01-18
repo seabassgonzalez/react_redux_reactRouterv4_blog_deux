d947accb5acc9ae8e744ffdfd795f1ae4b1a5624
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-17 21:39:29 -0800

    Import connect and fetchPost

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -10,6 +10,8 @@
 // export default use connect()(PostsShow)
 
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
 	render(){
