5a9ff792fe8283afe25eb0e975ddef372a1dc499
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-17 17:05:47 -0800

    Import FETCH_POST action creator, implement case for fetch_post, use ...state to maintain posts already in state

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,17 +1,21 @@
-// import FETCH_POSTS from actions
+// import FETCH_POSTS and FETCH_POST from actions
 
 // export default function taking state defaulting it to an object and an action
 	// switch statement for action types
+		// FETCH_POST
+			// return
 		// FETCH_POSTS
 			// return _.mapKeys on array action.payload.data, use id prop to create id
 		// default case
 			// return state object
 
 import _ from 'lodash';
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POSTS, FETCH_POST } from '../actions';
 
 export default function(state = {}, action){
 	switch(action.type){
+		case FETCH_POST:
+			return { ...state };
 		case FETCH_POSTS:
 			return _.mapKeys(action.payload.data, 'id');
 		default:
